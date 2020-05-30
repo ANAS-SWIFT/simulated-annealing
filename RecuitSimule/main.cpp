6df9e5132bc376ff9103e9d8f27f595c1da72901
ANAS <anasmoumtaji98@gmail.com>
2020-05-30 19:04:04 +0000

    Initial Commit

---
+++ RecuitSimule/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RecuitSimule
+//
+//  Created by ANAS on 5/30/20.
+//  Copyright © 2020 ANAS. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
